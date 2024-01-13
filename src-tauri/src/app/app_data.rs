c166af1850d312c3a287de7b692b5335ea444ebb
Greenman999 <mail@greenman999.de>
2024-01-13 12:39:45 +0100

    feat: add keyring storing for account credentials

--- src-tauri/src/app/app_data.rs
+++ src-tauri/src/app/app_data.rs
@@ -1,9 +1,12 @@
 use std::{collections::HashMap, path::Path};
 use std::path::PathBuf;
+use core::option::Option;
 
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use tokio::fs;
+use keyring::{Entry as KeyringEntry, Result as KeyringResult};
+//use serde::de::Unexpected::Option;
 
 use crate::app::api::LoginData;
 use crate::LAUNCHER_DIRECTORY;
@@ -43,12 +46,78 @@ pub(crate) struct LauncherOptions {
 impl LauncherOptions {
     pub async fn load(app_data: &Path) -> Result<Self> {
         // load the options from the file
-        Ok(serde_json::from_slice::<Self>(&fs::read(app_data.join("options.json")).await?)?)
-    }
+        let options: LauncherOptions = serde_json::from_slice::<Self>(&fs::read(app_data.join("options.json")).await?)?;
 
+        // load all tokens from keyring
+        let service = "noriskclient-launcher";
+        let mut accounts = options.accounts.clone();
+        for account in &mut accounts {
+            let uuid = account.uuid.clone();
+            let keyring_mc_token = KeyringEntry::new(service, &*format!("{}-{}", uuid, "mcToken"))?;
+            let keyring_access_token = KeyringEntry::new(service, &*format!("{}-{}", uuid, "accessToken"))?;
+            let keyring_refresh_token = KeyringEntry::new(service, &*format!("{}-{}", uuid, "refreshToken"))?;
+            let keyring_norisk_token = KeyringEntry::new(service, &*format!("{}-{}", uuid, "noriskToken"))?;
+            let keyring_experimental_token = KeyringEntry::new(service, &*format!("{}-{}", uuid, "experimentalToken"))?;
+            account.mc_token = keyring_mc_token.get_password().unwrap();
+            account.access_token = keyring_access_token.get_password().unwrap();
+            account.refresh_token = keyring_refresh_token.get_password().unwrap();
+            account.norisk_token = keyring_norisk_token.get_password().unwrap();
+            account.experimental_token = Some(keyring_experimental_token.get_password().unwrap());
+        }
+
+        let mut modified_options = options.clone();
+        modified_options.accounts = accounts;
+
+        Ok(modified_options)
+    }
     pub async fn store(&self, app_data: &Path) -> Result<()> {
         // store the options in the file
-        fs::write(app_data.join("options.json"), serde_json::to_string_pretty(&self)?).await?;
+        let accounts = &self.accounts.clone();
+        // for each LoginData, store all tokens in keyring
+        let service = "noriskclient-launcher";
+        for account in accounts {
+            let uuid = account.uuid.clone();
+            let keyring_mc_token = KeyringEntry::new(service, &*format!("{}-{}", uuid, "mcToken"))?;
+            let keyring_access_token = KeyringEntry::new(service, &*format!("{}-{}", uuid, "accessToken"))?;
+            let keyring_refresh_token = KeyringEntry::new(service, &*format!("{}-{}", uuid, "refreshToken"))?;
+            let keyring_norisk_token = KeyringEntry::new(service, &*format!("{}-{}", uuid, "noriskToken"))?;
+            let keyring_experimental_token = KeyringEntry::new(service, &*format!("{}-{}", uuid, "experimentalToken"))?;
+            keyring_mc_token.set_password(account.mc_token.clone().as_str()).unwrap();
+            keyring_access_token.set_password(account.access_token.clone().as_str()).unwrap();
+            keyring_refresh_token.set_password(account.refresh_token.clone().as_str()).unwrap();
+            keyring_norisk_token.set_password(account.norisk_token.clone().as_str()).unwrap();
+            keyring_experimental_token.set_password(account.experimental_token.clone().unwrap().as_str()).unwrap();
+        }
+
+        // remove all tokens from LoginData
+        let mut modified_accounts = Vec::new();
+        for account in &mut accounts.clone() {
+            modified_accounts.push(LoginData {
+                uuid: account.uuid.clone(),
+                username: account.username.clone(),
+                mc_token: String::new(),
+                access_token: String::new(),
+                refresh_token: String::new(),
+                norisk_token: String::new(),
+                experimental_token: None::<String>
+            });
+        }
+        let modified_options: LauncherOptions = LauncherOptions {
+            keep_launcher_open: self.keep_launcher_open.clone(),
+            experimental_mode: self.experimental_mode.clone(),
+            data_path: self.data_path.clone(),
+            memory_percentage: self.memory_percentage.clone(),
+            custom_java_path: self.custom_java_path.clone(),
+            custom_java_args: self.custom_java_args.clone(),
+            theme: self.theme.clone(),
+            latest_branch: self.latest_branch.clone(),
+            latest_dev_branch: self.latest_dev_branch.clone(),
+            current_uuid: self.current_uuid.clone(),
+            accounts: modified_accounts,
+            concurrent_downloads: self.concurrent_downloads.clone()
+        };
+
+        fs::write(app_data.join("options.json"), serde_json::to_string_pretty(&modified_options)?).await?;
         Ok(())
     }
 
@@ -86,9 +155,9 @@ impl Default for LauncherOptions {
             custom_java_path: String::new(),
             custom_java_args: String::new(),
             theme: theme.to_string(),
-            latest_branch: None,
-            latest_dev_branch: None,
-            current_uuid: None,
+            latest_branch: None::<String>,
+            latest_dev_branch: None::<String>,
+            current_uuid: None::<String>,
             accounts: Vec::new(),
             concurrent_downloads: 10
         }
